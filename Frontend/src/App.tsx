527d17a3f7b95f3fabec1de7e11083cb36e9d0ff
Vansh Reddy <68335743+vanshreddy@users.noreply.github.com>
2023-10-13 17:16:53 -0400

    added landing page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
+import React from 'react';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Box
+} from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
   return (
-    <>
-      
-    </>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+    <AppBar >
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          News
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+  </Box>
+  );
 }
 
-export default App
+export default App;
