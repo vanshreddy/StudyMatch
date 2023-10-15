2264b9a1407b722a7a5599951f7a5aae1c6e6cdf
Sidharth4032 <sidharth.eage@gmail.com>
2023-10-14 23:43:45 -0400

    Backed password creation

--- Backend/app.js
+++ Backend/app.js
@@ -1,35 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
-const app = express();  //Create new instance
-const PORT = process.env.PORT || 3000; //Declare the port number
-app.use(express.json()); //allows us to access request body as req.body
-app.use(morgan("dev"));  //enable incoming request logging in dev mode
- 
-//Define the endpoint
-app.get("/ping", (req, res) => {  
-  return res.send({
-    status: "Healthy",
-  });
-});
-app.listen(PORT, () => {
-  console.log("Server started listening on port : ", PORT);
-});
+const mysql = require("mysql2");
 
-//Error Handling
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something went wrong!");
-});
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-const express = require("express");
-const morgan = require("morgan");
-const mysql = require("mysql2"); // Import the mysql2 package
-const app = express(); // Create a new instance
-const PORT = process.env.PORT || 3000; // Declare the port number
-app.use(express.json()); // Allows us to access request body as req.body
-app.use(morgan("dev")); // Enable incoming request logging in dev mode
+app.use(express.json());
+app.use(morgan("dev"));
 
-// Define the database connection configuration
 const dbConfig = {
   host: "localhost",
   user: "sune2",
@@ -37,38 +15,59 @@ const dbConfig = {
   database: "database_name",
 };
 
-// Create a MySQL connection pool
 const pool = mysql.createPool(dbConfig);
 
-// Error handling for the database connection
 pool.getConnection((err, connection) => {
   if (err) {
     console.error("Error connecting to MySQL:", err);
     return;
   }
   console.log("Connected to MySQL");
-  connection.release(); // Release the connection back to the pool
+  connection.release();
 });
 
-// Define the endpoint
 app.get("/ping", (req, res) => {
-  return res.send({
-    status: "Healthy",
-  });
+  return res.send({ status: "Healthy" });
 });
 
-// Example route that queries the database
-app.get("/users", (req, res) => {
-  pool.query("SELECT * FROM users", (err, results) => {
+// User registration endpoint
+app.post("/register", (req, res) => {
+  const { username, password } = req.body;
+
+  const query = "INSERT INTO users (username, password) VALUES (?, ?)";
+  pool.query(query, [username, password], (err) => {
     if (err) {
       console.error("Error executing MySQL query:", err);
       return res.status(500).send("Database error");
     }
-    res.json(results);
+    res.send({ status: "User registered successfully!" });
   });
+});
+
+// User login endpoint
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  const query = "SELECT * FROM users WHERE username = ? AND password = ?";
+  pool.query(query, [username, password], (err, results) => {
+    if (err) {
+      console.error("Error executing MySQL query:", err);
+      return res.status(500).send("Database error");
+    }
+
+    if (results.length === 0) {
+      return res.status(401).send("Invalid username or password");
+    }
+
+    res.send({ status: "Login successful!" });
+  });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
 });
 
 app.listen(PORT, () => {
   console.log("Server started listening on port:", PORT);
 });
-
