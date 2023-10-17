588ada3694445a2072b6bf50d05742782e6fe058
Vansh Reddy <68335743+vanshreddy@users.noreply.github.com>
2023-10-17 18:35:52 -0400

    added dark theme

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, } from "react-router-dom";
+
+
+import { LandingPage } from './pages/LandingPage/main';
+
 
 function App() {
   return (
-    <>
+    <Routes>
+    <Route path="/">
+      <Route index element={<LandingPage />} />
       
-    </>
+    </Route>
+  </Routes>
   )
 }
 
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  brand: {
+    50: "#ecefff",
+    100: "#cbceeb",
+    200: "#a9aed6",
+    300: "#888ec5",
+    400: "#666db3",
+    500: "#4d5499",
+    600: "#3c4178",
+    700: "#2a2f57",
+    800: "#181c37",
+    900: "#080819"
+  }
+};
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: false
+};
+
+const theme = extendTheme({ colors, config });
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+      <ChakraProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    </ChakraProvider>,
+
   </React.StrictMode>,
 )
