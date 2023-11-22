45834530721806afe52a097adf4f56b1c1f945df
Vansh Reddy <68335743+vanshreddy@users.noreply.github.com>
2023-11-22 14:27:56 -0500

    added login route

--- Backend/controllers/users.controller.js
+++ Backend/controllers/users.controller.js
@@ -48,6 +48,40 @@ const createUser = catchAsync(async (req, res) => {
 });
 
 
+const loginUser = catchAsync(async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+      // Check if user exists
+      const user = await prisma.user.findUnique({
+        where: {
+          email: email,
+        },
+      });
+  
+      if (!user) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+      }
+  
+      // Compare password
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+      }
+  
+      // Create token
+      const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+        expiresIn: '1h',
+      });
+  
+      res.json({ token });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+
+});
+
+
 module.exports = {
     createUser,
   };
