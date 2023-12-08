6d96a4728f69fa60e82a979e767689e134d20f6d
esun12 <elvissun811@gmail.com>
2023-12-08 18:57:13 -0500

    Terms of Service Text

---
+++ Frontend/src/components/termsOfService/main.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const TermsOfService = () => {
+  return (
+    <div>
+      <h1>Terms of Service</h1>
+      <p>
+[Welcome to StudyMatch! By using our website, services, or applications (collectively, "Services"), you agree to be bound by these Terms of Service ("Terms"). Please read them carefully.
+
+1. Acceptance of Terms
+By accessing or using our Services, you confirm your acceptance of these Terms and agree to be bound by them. If you do not agree to these Terms, you must not access or use our Services.
+
+2. Changes to Terms
+We reserve the right to modify these Terms at any time. We will notify you of any changes by posting the new Terms on this page. Your continued use of the Services following any changes indicates your acceptance of the new Terms.
+
+3. Use of Our Services
+Eligibility: You must be at least 13 years old to use our Services.
+Account Responsibility: You are responsible for safeguarding your account information and for all activities that occur under your account.
+Acceptable Use: You agree not to misuse our Services or help anyone else do so.
+4. Content and Conduct
+User-Generated Content: You retain rights to the content you create and share on StudyMatch, but you grant us a license to use, store, and share this content.
+Prohibited Conduct: You agree not to engage in conduct that is harmful, fraudulent, deceptive, or offensive.
+5. Intellectual Property Rights
+The content and design of our Services are protected by intellectual property rights owned by us or our licensors. You may not use such content without our express permission.
+
+6. Disclaimers
+Our Services are provided "as is" without any warranties, either express or implied. We do not guarantee the availability, accuracy, or reliability of our Services.
+
+7. Limitation of Liability
+To the fullest extent permitted by law, StudyMatch shall not be liable for any indirect, incidental, special, consequential, or punitive damages, or any loss of profits or revenues, whether incurred directly or indirectly.
+
+8. Termination
+We may suspend or terminate your access to our Services if you violate these Terms or for any other reason at our discretion.
+
+9. Governing Law
+These Terms shall be governed by and construed in accordance with the laws of the jurisdiction in which our company is registered, without regard to its conflict of law principles.
+
+10. Contact Us
+For any questions about these Terms, please contact us]
+      </p>}
+    </div>
+  );
+};
+
+export default TermsOfService;
